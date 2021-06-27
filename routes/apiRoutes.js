cd06ce4ee7a432e6e208f2c2d80a4b47f1ea898b
Heather Bjoin <h.m.bjoin@gmail.com>
2021-06-27 15:46:25 -0500

    removing the delete that doesn't work

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
+let noteList = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
 
 module.exports = (app) => {
-    let noteList = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
+
 
     app.get("/api/notes", (req,res) => {
         return res.json(noteList);
@@ -12,7 +13,6 @@ module.exports = (app) => {
         const newNoteBtn = req.body;
         newNoteBtn.id = uuidv4();
         console.log('newNoteBtn Id', JSON.stringify(newNoteBtn.id))
-        let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
 
         noteList.push(newNoteBtn);
 
@@ -21,25 +21,10 @@ module.exports = (app) => {
         res.json(noteList);
     });
 
-    app.delete(`api/notes`, (req, res) => {
-        const deleteNote = req.body.id;
-        console.log('deleteNote', deleteNote);
 
-        fs.readFile('.db/db.json', (err, data) => {
-            if (err) throw err;
-
-            dbData = JSON.parse(data);
-
-            for(let i = 0; i < dbData.length; i++){
-                if(dbData[i].id === Number(deleteNotes)){
-                    dbData.splice([i], 1);
-                }
-            }
-            stringData = JSON.stringify(dbData);
-            fs.writeFile('.db/db.json', stringData, (err,data) => {
-                if (err) throw err;
-            });
-        });
-        res.status(204).send();
-    });
+    // app.delete(`api/notes/:id`, (req, res) => {
+    //     const deleteNote = req.params.id;
+    //     console.log('deleteNote', deleteNote);
+    //     res.json(req.body);
+    // });
 }
